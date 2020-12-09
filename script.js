2a7ec1fbb62d4d4d5482d4863a1f44b937f37c6b
ashish kumar kashyap <er.ashish455@gmail.com>
2020-12-09 17:41:12 +0530

    hochpoch

---
+++ script.js
@@ -0,0 +1,43 @@
+var lengthof = $(".slides").siblings().length;
+// var counter = 1
+//     // $(".slider img").siblings().removeClass("nodisplay")
+
+// // function slideshow() {
+// //     console.log($("#" + counter))
+// //     $("#" + counter).removeClass('nodisplay')
+// //     setTimeout(function() {
+// //         $('#' + counter).addClass("nodisplay")
+
+// //     }, 1000)
+// //     counter += 1
+
+// // }
+
+// function slideshow() {
+//     $()
+// }
+
+
+// $(document).ready(setInterval(slideshow, 8000))
+
+
+
+var counter = 1;
+// debugger
+// setInterval(function() {
+//     console.log(counter)
+//     console.log('remove class', counter)
+//     $('#' + counter).removeClass('nodisplay')
+//     setTimeout(function() {
+//         console.log('add class', counter - 1)
+//         console.log("hata dia")
+
+//         $('#' + (counter - 1)).addClass('nodisplay')
+//         counter += 1
+//         if (counter == 6) {
+//             counter = 1
+//         }
+//     }, 2500)
+
+
+// }, 2500)
