d0dbeaacaeb8e84b097815fe57de59c0086fdac1
ashish kumar kashyap <er.ashish455@gmail.com>
2020-12-10 18:15:05 +0530

    asdf

--- script.js
+++ script.js
@@ -1,56 +1,5 @@
-// var togle = false;
-
-
-// let counter1 = 6;
-// let counter2 = 2;
-// var b;
-
-// function janeka() {
-//     console.log(counter1)
-
-//     $('#' + counter1).fadeOut('Slow');
-//     counter1--
-//     console.log(counter1)
-
-// }
-
-// function aaneka() {
-//     console.log(counter2)
-
-//     $('#' + counter2).fadeIn('Slow');
-//     counter2++;
-
-// }
-
-// function toggle() {
-//     var a = setInterval(janeka, 6000);
-//     setTimeout(function() {
-//         clearInterval(a),
-//             counter1 = 6
-//         b = setInterval(aaneka, 6000);
-
-//     }, 32000);
-
-//     setTimeout(function() {
-//         clearInterval(b)
-//         counter2 = 1
-//     }, 68000)
-
-// }
-// toggle()
-// let asd = setInterval(toggle, 60000)
-
-// $('.next').click(function() {
-
-//     let current = (counter1)
-//     console.log(current)
-//     $('#' + current).fadeOut()
-//     console.log('aaya hai')
-
-
-// });
 var x = 0;
-var q = setInterval(pagination, 2000)
+
 
 $(document).ready(function() {
 
@@ -68,12 +17,15 @@ suspend = false;
 
 let a = setInterval(function() {
     if (!suspend) {
+
         $('figure').css('left', -x + '%')
+        pagination()
         console.log(x)
         x += 100
         if (x == 600) {
             x = 0
         }
+
     }
 }, 2000)
 
@@ -92,30 +44,25 @@ function pagination() {
     if (x == 0) {
 
         $('#1').attr('checked', true)
-        $('#1').siblings().attr('checked', false)
-
-
-
 
     } else if (x == 100) {
         $('#2').attr('checked', true)
-        $('#2').siblings().attr('checked', false)
 
     } else if (x == 200) {
         $('#3').attr('checked', true)
-        $('#3').siblings().attr('checked', false)
+
 
     } else if (x == 300) {
         $('#4').attr('checked', true)
-        $('#4').siblings().attr('checked', false)
+
 
     } else if (x == 400) {
         $('#5').attr('checked', true)
-        $('#5').siblings().attr('checked', false)
+
 
     } else if (x == 500) {
         $('#6').attr('checked', true)
-        $('#6').siblings().attr('checked', false)
+
     }
 
 
