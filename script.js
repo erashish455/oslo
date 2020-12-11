f2ac2aacae8ebee36523ae51a5e19c80a181552c
ashish kumar kashyap <er.ashish455@gmail.com>
2020-12-11 10:49:10 +0530

    pagination is working properly

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ var x = 0;
 
 
 $(document).ready(function() {
+    pagination()
 
     $(".btn-next").click(function() {
         x = x <= 500 ? x + 100 : 0;
         $("figure").css("left", -x + "%");
-        console.log(x);
+        pagination()
     });
     $(".btn-prev").click(function() {
         x = x >= 100 ? x - 100 : 600;
         $("figure").css("left", -x + "%");
+        pagination()
     });
 });
 suspend = false;
@@ -30,38 +32,50 @@ let a = setInterval(function() {
 }, 2000)
 
 
-$('figure').mouseenter(function() {
+$('figure, .rbtn, .btn-prev,.btn-next ').mouseenter(function() {
     console.log("mouse aaya")
     suspend = true;
 });
-$('figure').mouseleave(function() {
+$('figure, .rbtn, .btn-prev,.btn-next ').mouseleave(function() {
     console.log('mouseleave')
     suspend = false;
 });
 
 
 function pagination() {
+    $('.rbtn').siblings().removeAttr('checked')
     if (x == 0) {
 
+
         $('#1').attr('checked', true)
+
 
     } else if (x == 100) {
         $('#2').attr('checked', true)
 
+
     } else if (x == 200) {
         $('#3').attr('checked', true)
+
 
 
     } else if (x == 300) {
         $('#4').attr('checked', true)
 
 
+
     } else if (x == 400) {
         $('#5').attr('checked', true)
 
 
+
     } else if (x == 500) {
         $('#6').attr('checked', true)
+
+
+    } else if (x == 600) {
+        $('#7').attr('checked', true)
+
 
     }
 
